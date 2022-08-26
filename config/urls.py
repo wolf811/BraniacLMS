218367b8e07c3cf7b41be91470ad3fb5c1cefc20
wolf811 <wolf811@gmail.com>
2022-08-26 15:17:26 +0300

    add black

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,7 @@ from django.views.generic import RedirectView
 from mainapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url="mainapp/")),
-    path('mainapp/', include("mainapp.urls")),
-
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="mainapp/")),
+    path("mainapp/", include("mainapp.urls")),
 ]

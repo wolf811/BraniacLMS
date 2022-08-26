f0deb6201bfc778dbdbd56ac6004bf4428dbc963
wolf811 <wolf811@gmail.com>
2022-08-26 16:04:18 +0300

    add les_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,6 +9,13 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        return context
 
 
 class CoursesPageView(TemplateView):

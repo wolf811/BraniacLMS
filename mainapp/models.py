a3a1ac51ff616cf690b5e6776c0030229f677b75
wolf811 <wolf811@gmail.com>
2022-09-01 16:50:00 +0300

    fix les4

--- mainapp/models.py
+++ mainapp/models.py
@@ -26,7 +26,12 @@ class News(models.Model):
         self.save()
 
 
+class CoursesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
 class Courses(models.Model):
+    objects = CoursesManager()
     name = models.CharField(max_length=256, verbose_name="Name")
     description = models.TextField(
         verbose_name="Description", blank=True, null=True
